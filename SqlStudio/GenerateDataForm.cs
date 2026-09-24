e5dd197b9aaa16edccde0cbdbd1fd3f792f2e27e
bard hustveit <bard.hustveit@gmail.com>
2026-09-24 20:54:51 +0200

    update all nuget packages

--- SqlStudio/GenerateDataForm.cs
+++ SqlStudio/GenerateDataForm.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.Common;
-using System.Data.Entity.Core.Mapping;
 using System.Drawing;
 using System.Linq;
 using System.Linq.Expressions;
